f5b4e3d428e967db94d9a4eadfcc892591988378
谭棋友 <tanqiyou@gmail.com>
2020-11-19 16:11:51 +0800

    增加post请求方式

--- src/main/java/org/sysethereum/agents/service/rest/GetSyscoinRPCHandler.java
+++ src/main/java/org/sysethereum/agents/service/rest/GetSyscoinRPCHandler.java
@@ -4,14 +4,15 @@ import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpsExchange;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 import org.sysethereum.agents.core.syscoin.SyscoinRPCClient;
 import org.sysethereum.agents.util.RestError;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.*;
 
 @Service
 @Slf4j(topic = "GetSyscoinRPCHandler")
@@ -29,31 +30,63 @@ public class GetSyscoinRPCHandler extends CommonHttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         HttpsExchange httpsExchange = (HttpsExchange) httpExchange;
         if (setOriginAndHandleOptionsMethod(httpsExchange)) return;
-
-        LinkedHashMap<String, String> params = queryToMap(httpsExchange.getRequestURI().getQuery());
         String response;
-        try {
-            String method = params.get("method");
-            params.remove("method");
-            if (method.equals("signrawtransactionwithkey")) {
-                String hexstring = params.get("hexstring");
-                String privkey = params.get("privkeys");
-                String[] split = privkey.split(",");
-                List<String> privkeList = new ArrayList<>();
-                for (String s : split) {
-                    privkeList.add(s);
-                }
-                ArrayList<Object> paramList = new ArrayList<>();
-                paramList.add(hexstring);
-                paramList.add(privkeList);
+
+        if ("post".equalsIgnoreCase(httpsExchange.getRequestMethod())) {
+            String postString = IOUtils.toString(httpsExchange.getRequestBody());
+            Map<String,String> postInfo = formData2Dic(postString);
+            try {
+                String method = postInfo.get("method");
+                postInfo.remove("method");
+                ArrayList<Object> paramList = new ArrayList<>(postInfo.values());
                 response = syscoinRPCClient.makeCoreCall(method, paramList);
-            } else {
-                ArrayList<Object> paramList = new ArrayList<>(params.values());
-                response = syscoinRPCClient.makeCoreCall(method, paramList);
+            } catch (Exception e) {
+                response = gson.toJson(new RestError(e.toString()));
             }
-        } catch (Exception e) {
-            response = gson.toJson(new RestError(e.toString()));
+        } else {
+            LinkedHashMap<String, String> params = queryToMap(httpsExchange.getRequestURI().getQuery());
+            try {
+                String method = params.get("method");
+                params.remove("method");
+                if (method.equals("signrawtransactionwithkey")) {
+                    String hexstring = params.get("hexstring");
+                    String privkey = params.get("privkeys");
+                    String[] split = privkey.split(",");
+                    List<String> privkeList = new ArrayList<>();
+                    for (String s : split) {
+                        privkeList.add(s);
+                    }
+                    ArrayList<Object> paramList = new ArrayList<>();
+                    paramList.add(hexstring);
+                    paramList.add(privkeList);
+                    response = syscoinRPCClient.makeCoreCall(method, paramList);
+                } else {
+                    ArrayList<Object> paramList = new ArrayList<>(params.values());
+                    response = syscoinRPCClient.makeCoreCall(method, paramList);
+                }
+            } catch (Exception e) {
+                response = gson.toJson(new RestError(e.toString()));
+            }
         }
         writeResponse(httpsExchange, response);
+    }
+
+    public static Map<String,String> formData2Dic(String formData ) {
+        Map<String,String> result = new HashMap<>();
+        if(formData== null || formData.trim().length() == 0) {
+            return result;
+        }
+        final String[] items = formData.split("&");
+        Arrays.stream(items).forEach(item ->{
+            final String[] keyAndVal = item.split("=");
+            if( keyAndVal.length == 2) {
+                try{
+                    final String key = URLDecoder.decode( keyAndVal[0],"utf8");
+                    final String val = URLDecoder.decode( keyAndVal[1],"utf8");
+                    result.put(key,val);
+                }catch (UnsupportedEncodingException e) {}
+            }
+        });
+        return result;
     }
 }
